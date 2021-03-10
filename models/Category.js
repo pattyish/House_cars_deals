d2d9226da1443c74cbbc2ef661ad4a4f8ecf621c
pattyish <patrickishimwe10@gmail.com>
2021-03-10 14:48:02 +0200

    category, sub_category and advert type models

--- models/Category.js
+++ models/Category.js
@@ -1,18 +1,29 @@
-const mongoose = require('mongoose');
+import moment from 'moment';
 
-const categorySchema = new mongoose.Schema({
-  categoryName: {
-    type: String,
-    required: true,
-  },
-  createdAt: {
-    type: Date,
-    default: Date.now,
-  },
-  status: {
-    type: Boolean,
-    default: true,
-  },
-});
+export const category = (category) => {
+  return {
+    categ_name: category.categ_name,
+    user_id: category.user_id,
+    createdAt: moment.defaultFormat(),
+    status: true,
+  };
+};
 
-module.exports = Category = mongoose.model('Category', categorySchema);
+export const subCategory = (subCategory) => {
+  return {
+    sub_categ_name: subCategory.categ_name,
+    user_id: subCtegory.user_id,
+    categ_id: subCategory.categ_id,
+    createdAt: moment.defaultFormat(),
+    status: true,
+  };
+};
+
+export const advertType = (advertType) => {
+  return {
+    advert_type_name: advertType.advertType,
+    user_id: advertType.user_id,
+    createdAt: moment.defaultFormat(),
+    status: true,
+  };
+};
