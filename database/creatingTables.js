4d58fe228302825ef5f2f2f73ff6555e1ca7198e
pattyish <patrickishimwe10@gmail.com>
2021-03-10 12:14:35 +0200

    car, land, and realestate tables

--- database/creatingTables.js
+++ database/creatingTables.js
@@ -7,30 +7,33 @@ const TableCreated = pool.query(
   DROP TABLE IF EXISTS sub_category;
   DROP TABLE IF EXISTS category;
   DROP TABLE IF EXISTS advert_type;
+  DROP TABLE IF EXISTS car_details;
+  DROP TABLE IF EXISTS land_details;
+  DROP TABLE IF EXISTS realest_details;
   DROP TABLE IF EXISTS properties;
   DROP TABLE IF EXISTS realestate_requests;
   DROP TABLE IF EXISTS vehicle_requests;
-    CREATE TABLE users(
-      user_id BIGSERIAL PRIMARY KEY,
-      firstname VARCHAR(200) NOT null,
-      lastname VARCHAR(50) NOT null unique,
-      email VARCHAR(150) NOT null unique,
-      password VARCHAR(150) NOT null,
-      phone VARCHAR(50) NOT null unique,
-      isAdmin boolean,
-      createdAt DATETIME null,
-      status VARCHAR(30) NOT null
-    );
-    CREATE TABLE user_profile(
-      user_prof_id BIGSERIAL PRIMARY KEY,
-      user_id BIGSERIAL,
-      Phone_nbr2 VARCHAR(50) NOT null unique,
-      country VARCHAR(50),
-      city VARCHAR(100),
-      district VARCHAR(100), 
-      sector VARCHAR(150),
-      bio TEXT 
-  );
+CREATE TABLE users(
+  user_id BIGSERIAL PRIMARY KEY,
+  firstname VARCHAR(200) NOT null,
+  lastname VARCHAR(50) NOT null unique,
+  email VARCHAR(150) NOT null unique,
+  password VARCHAR(150) NOT null,
+  phone VARCHAR(50) NOT null unique,
+  isAdmin boolean,
+  createdAt DATETIME null,
+  status VARCHAR(30) NOT null
+);
+CREATE TABLE user_profile(
+  user_prof_id BIGSERIAL PRIMARY KEY,
+  user_id BIGSERIAL,
+  Phone_nbr2 VARCHAR(50) NOT null unique,
+  country VARCHAR(50),
+  city VARCHAR(100),
+  district VARCHAR(100), 
+  sector VARCHAR(150),
+  bio TEXT 
+);
   CREATE TABLE category(
     categ_id BIGSERIAL PRIMARY KEY,
     categ_name VARCHAR(50) NOT null,
@@ -60,6 +63,7 @@ CREATE TABLE properties(
   main_image VARCHAR(200) NOT null,
   advert_type_id BIGSERIAL,
   sub_categ_id BIGSERIAL,
+  createdAt DATETIME null,
   createdAt DATETIME null,
   status boolean
 );
@@ -94,10 +98,6 @@ CREATE TABLE vehicle_requests(
   min_amount money NOT null,
   max_amount money NOT null,
   time_need VARCHAR(50) NOT null,
-  make VARCHAR(50),
-  model VARCHAR(50),
-  fuel VARCHAR(100),
-  transmission VARCHAR(100),
   firstname VARCHAR(50) NOT null,
   lastname VARCHAR(50) NOT null,
   email  VARCHAR(100),
@@ -105,6 +105,35 @@ CREATE TABLE vehicle_requests(
   createdAt DATETIME null,
   expiredDate DATETIME null,
   status boolean
+);
+CREATE TABLE realest_details(
+  realest_id BIGSERIAL PRIMARY KEY,
+  property_id BIGSERIAL,
+  bedroom INTEGER,
+  bathroom INTEGER,
+  floors INTEGER,
+  living_area INTEGER,
+  plot_size INTEGER,
+  parking boolean,
+  TEXT NOT null,
+  longitude VARCHAR(100),
+  latutide VARCHAR(100)
+);
+CREATE TABLE land_details(
+  land_id BIGSERIAL PRIMARY KEY,
+  property_id BIGSERIAL,
+  plot_size INTEGER,
+  longitude VARCHAR(100),
+  latutide VARCHAR(100)
+);
+CREATE TABLE car_details(
+  car_id BIGSERIAL PRIMARY KEY,
+  property_id BIGSERIAL,
+  make VARCHAR(100),
+  model VARCHAR(100),
+  year VARCHAR(20),
+  transmission VARCHAR(20),
+  fuel VARCHAR(50)
 );
     INSERT INTO employees 
     (empl_name, nationalId, phone, email, dob, position, status)
